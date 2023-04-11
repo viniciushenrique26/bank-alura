d35f5e8eb540e15dd1d98376935bb02086c07730
Vinícius Slavov <viniciusslavov@gmail.com>
2023-04-11 18:09:44 -0300

    validação de cpf funcionando

---
+++ js/valida-cpf.js
@@ -0,0 +1,66 @@
+//Export default vai definir esse função como a padrão do arquivo,
+//quando o arquivo for chamado, ela que será retornada.
+
+export default function umCPF(campo) {
+  const cpf = campo.value.replace(/\.|-/g, '');
+  if (
+    validaNumerosRepetidos(cpf) ||
+    validaPrimeiroDigito(cpf) ||
+    validaSegundoDigito(cpf)
+  ) {
+    console.log('Esse Cpf não existe');
+  } else {
+    console.log('Esse cpf existe');
+  }
+}
+
+function validaNumerosRepetidos(cpf) {
+  const numerosRepetidos = [
+    '00000000000',
+    '11111111111',
+    '22222222222',
+    '33333333333',
+    '44444444444',
+    '55555555555',
+    '66666666666',
+    '77777777777',
+    '88888888888',
+    '99999999999',
+  ];
+
+  return numerosRepetidos.includes(cpf);
+}
+
+function validaPrimeiroDigito(cpf) {
+  let soma = 0;
+  let multiplicador = 10;
+
+  for (let tamanho = 0; tamanho < 9; tamanho++) {
+    soma += cpf[tamanho] * multiplicador;
+    multiplicador--;
+  }
+  soma = (soma * 10) % 11;
+
+  if (soma == 10 || soma == 1) {
+    soma = 0;
+  }
+
+  return soma != cpf[9];
+}
+
+function validaSegundoDigito(cpf) {
+  let soma = 0;
+  let multiplicador = 11;
+
+  for (let tamanho = 0; tamanho < 10; tamanho++) {
+    soma += cpf[tamanho] * multiplicador;
+    multiplicador--;
+  }
+  soma = (soma * 10) % 11;
+
+  if (soma == 10 || soma == 1) {
+    soma = 0;
+  }
+
+  return soma != cpf[10];
+}
